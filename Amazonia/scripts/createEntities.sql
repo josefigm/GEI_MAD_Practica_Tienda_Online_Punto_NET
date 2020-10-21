f4126a029f77f33ca89824a7a9e6a550038be72c
José <jose.figueirasm@udc.es>
2020-10-21 16:02:32 +0200

    EDMX + Entidades
    + Entidades del modelo
    + Archivo EDMX & archivos relacionados

--- Amazonia/scripts/createEntities.sql
+++ Amazonia/scripts/createEntities.sql
@@ -1,8 +1,6 @@
-/******************************************************************************/
- /*** PATH to store the db files. This path must exists in the local system. ***/
- /******************************************************************************/
- DECLARE @Default_DB_Path as VARCHAR(64)  
- SET @Default_DB_Path = N'C:\SourceCode\DataBase\'
+ 
+DECLARE @Default_DB_Path as VARCHAR(64)  
+SET @Default_DB_Path = N'C:\SourceCode\DataBase\'
  
 USE [master]
 
@@ -27,7 +25,50 @@ GO
 
 /* Create Tables */
 
-CREATE TABLE User (
+/*  Drop Tables  if already exists  */
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[SaleLine]') AND type in ('U'))
+DROP TABLE [SaleLine]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Sale]') AND type in ('U'))
+DROP TABLE [Sale]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Product]') AND type in ('U'))
+DROP TABLE [Product]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[ClientCard]') AND type in ('U'))
+DROP TABLE [ClientCard]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[CommentLabel]') AND type in ('U'))
+DROP TABLE [CommentLabel]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Client]') AND type in ('U'))
+DROP TABLE [Client]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Card]') AND type in ('U'))
+DROP TABLE [Card]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Category]') AND type in ('U'))
+DROP TABLE [Category]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Comment]') AND type in ('U'))
+DROP TABLE [Comment]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('[Label]') AND type in ('U'))
+DROP TABLE [Label]
+GO
+
+USE [amazonia]
+
+CREATE TABLE Client (
 	login varchar(30),
 	password varchar(60) NOT NULL,
 	firstName varchar(30) NOT NULL,
@@ -37,10 +78,10 @@ CREATE TABLE User (
 	role tinyint NOT NULL,
 	language tinyint NOT NULL,
 
-	CONSTRAINT [PK_User] PRIMARY KEY (login)
+	CONSTRAINT [PK_Client] PRIMARY KEY (login)
 )
 
-PRINT N'Table User created.'
+PRINT N'Table Client created.'
 GO
 
 CREATE TABLE Card (
@@ -48,7 +89,7 @@ CREATE TABLE Card (
 	cvv varchar(3) NOT NULL,
 	expireDate date NOT NULL,
 	name varchar(100) NOT NULL,
-	default bit NOT NULL,
+	defaultCard bit NOT NULL,
 	type bit NOT NULL,
 
 	CONSTRAINT [PK_Card] PRIMARY KEY (number)
@@ -57,66 +98,32 @@ CREATE TABLE Card (
 PRINT N'Table Card created.'
 GO
 
-CREATE TABLE UserCard (
+CREATE TABLE ClientCard (
 	login varchar(30),
 	number varchar(16),
 
-	CONSTRAINT [PK_UserCard] PRIMARY KEY (login, number),
-	CONSTRAINT [FK_UserCardUser] FOREING KEY (login) REFERENCES User(login),
-	CONSTRAINT [FK_UserCardCard] FOREING KEY (number) REFERENCES Card(number)
+	CONSTRAINT [PK_ClientCard] PRIMARY KEY (login, number),
+	CONSTRAINT [FK_ClientCardClient] FOREIGN KEY (login) REFERENCES Client(login),
+	CONSTRAINT [FK_ClientCardCard] FOREIGN KEY (number) REFERENCES Card(number)
 )
 
 
-PRINT N'Table UserCard created.'
+PRINT N'Table ClientCard created.'
 GO
 
-CREATE TABLE Order (
+CREATE TABLE Sale (
 	id bigint IDENTITY(1,1),
 	date date NOT NULL,
 	address varchar(100) NOT NULL,
 	totalPrice float NOT NULL,
 	cardNumber varchar(16) NOT NULL,
 
-	CONSTRAINT [PK_Order] PRIMARY KEY (id),
-	CONSTRAINT [FK_OrderCard] FOREING KEY (cardNumber) REFERENCES Card(number)
+	CONSTRAINT [PK_Sale] PRIMARY KEY (id),
+	CONSTRAINT [FK_SaleCard] FOREIGN KEY (cardNumber) REFERENCES Card(number)
 )
 
 
-PRINT N'Table Order created.'
-GO
-
-CREATE TABLE Product (
-	id bigint IDENTITY(1,1),
-	name varchar(30) NOT NULL,
-	price float NOT NULL,
-	entryDate date NOT NULL,
-	stock long NOT NULL,
-	image varchar(100),
-	description varchar(1000),
-	categoryId bigint NOT NULL,
-
-	CONSTRAINT [PK_Product] PRIMARY KEY (id),
-	CONSTRAINT [FK_ProductCategory] FOREING KEY (categoryId) REFERENCES Category(id)
-
-)
-
-PRINT N'Table Product created.'
-GO
-
-CREATE TABLE OrderLine (
-	id bigint IDENTITY(1,1),
-	units long NOT NULL,
-	price float NOT NULL,
-	gift bit NOT NULL,
-	productId bigint NOT NULL,
-	orderId bigint NOT NULL,
-
-	CONSTRAINT [PK_OrderLine] PRIMARY KEY (id),
-	CONSTRAINT [FK_OrderLineProduct] FOREING KEY (productId) REFERENCES Product(id),
-	CONSTRAINT [FK_OrderLineOrder] FOREING KEY (orderId) REFERENCES Order(id)
-)
-
-PRINT N'Table OrderLine created.'
+PRINT N'Table Sale created.'
 GO
 
 CREATE TABLE Category (
@@ -129,14 +136,48 @@ CREATE TABLE Category (
 PRINT N'Table Category created.'
 GO
 
+CREATE TABLE Product (
+	id bigint IDENTITY(1,1),
+	name varchar(30) NOT NULL,
+	price float NOT NULL,
+	entryDate date NOT NULL,
+	stock bigint NOT NULL,
+	image varchar(100),
+	description varchar(1000),
+	categoryId bigint NOT NULL,
+
+	CONSTRAINT [PK_Product] PRIMARY KEY (id),
+	CONSTRAINT [FK_ProductCategory] FOREIGN KEY (categoryId) REFERENCES Category(id)
+
+)
+
+PRINT N'Table Product created.'
+GO
+
+CREATE TABLE SaleLine (
+	id bigint IDENTITY(1,1),
+	units bigint NOT NULL,
+	price float NOT NULL,
+	gift bit NOT NULL,
+	productId bigint NOT NULL,
+	saleId bigint NOT NULL,
+
+	CONSTRAINT [PK_SaleLine] PRIMARY KEY (id),
+	CONSTRAINT [FK_SaleLineProduct] FOREIGN KEY (productId) REFERENCES Product(id),
+	CONSTRAINT [FK_SaleLineSale] FOREIGN KEY (saleId) REFERENCES Sale(id)
+)
+
+PRINT N'Table SaleLine created.'
+GO
+
 /*
 CREATE TABLE ProductCategory (
 	categoryId bigint,
 	productId bigint,
 
 	CONSTRAINT [PK_ProductCategory] PRIMARY KEY (categoryId, productId),
-	CONSTRAINT [FK_ProductCategoryCategory] FOREING KEY (categoryId) REFERENCES Category(id),
-	CONSTRAINT [FK_ProductCategoryProduct] FOREING KEY (productId) REFERENCES Product(id)
+	CONSTRAINT [FK_ProductCategoryCategory] FOREIGN KEY (categoryId) REFERENCES Category(id),
+	CONSTRAINT [FK_ProductCategoryProduct] FOREIGN KEY (productId) REFERENCES Product(id)
 
 PRINT N'Table ProductCategory created.'
 GO
@@ -170,8 +211,8 @@ CREATE TABLE CommentLabel (
 	labelId bigint,
 
 	CONSTRAINT [PK_CommentLabel] PRIMARY KEY (commentId, labelId),
-	CONSTRAINT [FK_CommentLabelComment] FOREING KEY (commentId) REFERENCES Comment(id),
-	CONSTRAINT [FK_CommentLabelLabel] FOREING KEY (labelId) REFERENCES Label(id)
+	CONSTRAINT [FK_CommentLabelComment] FOREIGN KEY (commentId) REFERENCES Comment(id),
+	CONSTRAINT [FK_CommentLabelLabel] FOREIGN KEY (labelId) REFERENCES Label(id)
 )
 
 PRINT N'Table CommentLabel created.'
