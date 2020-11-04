a53c787ceb49854faf47424e26f54f231caa34bf
José <jose.figueirasm@udc.es>
2020-11-04 17:29:33 +0100

    Implementacion de DAO de productos

---
+++ Amazonia/Model/DAOs/CategoryDao/IProductDao.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.Amazonia.Model.DAOs.ProductDao
+{
+    public interface IProductDao : IGenericDao<Product, Int64>
+    {
+    }
+}
---
+++ Amazonia/Model/DAOs/CategoryDao/ProductDaoEntityFramework.cs
@@ -0,0 +1,11 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+
+
+namespace Es.Udc.DotNet.Amazonia.Model.DAOs.ProductDao
+{
+    public class ProductDaoEntityFramework :
+        GenericDaoEntityFramework<Product, Int64>, IProductDao
+    {
+    }
+}
