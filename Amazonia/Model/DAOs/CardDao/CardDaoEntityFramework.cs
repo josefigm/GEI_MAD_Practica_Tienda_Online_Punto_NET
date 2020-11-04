9c1a2d33d786672e95f7d8d82badd43d882d277c
MarcosGraña <marcos.grana@udc.es>
2020-11-04 17:48:14 +0100

    Implementación de DAO de card

---
+++ Amazonia/Model/DAOs/CardDao/CardDaoEntityFramework.cs
@@ -0,0 +1,13 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Es.Udc.DotNet.Amazonia.Model.DAOs.CardDao
+{
+    public class CardDaoEntityFramework :
+       GenericDaoEntityFramework<Card, Int64>, ICardDao
+    {
+    }
+}
---
+++ Amazonia/Model/DAOs/CardDao/ICardDao.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.Amazonia.Model.DAOs.CardDao
+{
+    public interface ICardDao : IGenericDao<Card, Int64>
+}
