7814cf91b48ddc4635c4b8775faf833c3c638565
MarcosGraña <marcos.grana@udc.es>
2020-11-04 17:55:48 +0100

    BugFix: Faltaban llaves en la interfaz del dao de Tarjeta

--- Amazonia/Model/DAOs/CardDao/CardDaoEntityFramework.cs
+++ Amazonia/Model/DAOs/CardDao/CardDaoEntityFramework.cs
@@ -1,8 +1,6 @@
 ﻿using Es.Udc.DotNet.ModelUtil.Dao;
 using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
+
 
 namespace Es.Udc.DotNet.Amazonia.Model.DAOs.CardDao
 {
--- Amazonia/Model/DAOs/CardDao/ICardDao.cs
+++ Amazonia/Model/DAOs/CardDao/ICardDao.cs
@@ -4,4 +4,6 @@ using Es.Udc.DotNet.ModelUtil.Dao;
 namespace Es.Udc.DotNet.Amazonia.Model.DAOs.CardDao
 {
     public interface ICardDao : IGenericDao<Card, Int64>
+    {
+    }
 }
